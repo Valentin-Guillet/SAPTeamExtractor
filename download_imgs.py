3f0899f1c49ed9d030ec29bed52889d975d3291c
Val95240 <Valentin.GUILLET@isae-supaero.fr>
2022-06-08 19:32:07 +0200

    Downloader: fix Cat + wiki downloads

--- download_imgs.py
+++ download_imgs.py
@@ -6,19 +6,55 @@ import os
 import shutil
 import subprocess
 
+import cv2
+import numpy as np
+from PIL import Image
+
 
 DATA_FILE = "data.json"
 WRONG_COMMITS = ["Bus", "Boar", "Dromedary"]
 
+SAP_WIKI_URL = {"Tabby Cat": "4/4c/TabbyCat.png",
+                "Garlic Armor": "c/cc/Garlic.png", }
+
+
+def download_from_wiki(img_name, dst_file, img_size):
+    print(f"Processing {img_name} (from wiki to {dst_file})")
+    revision = SAP_WIKI_URL[img_name]
+    img_url = f"https://static.wikia.nocookie.net/superautopets/images/{revision}"
+    download_cmd = ["wget", img_url, '-O', dst_file]
+    subprocess.run(download_cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    img = cv2.imread(dst_file, cv2.IMREAD_UNCHANGED)
+    h, w, _ = img.shape
+    if h > w:
+        before_col = np.zeros((h, (h - w) // 2, 4), np.uint8)
+        after_col = np.zeros((h, (h - w) // 2, 4), np.uint8)
+        img = np.hstack((before_col, img, after_col))
+
+    elif h < w:
+        before_row = np.zeros(((w - h), w // 2, 4), np.uint8)
+        after_row = np.zeros(((w - h), w // 2, 4), np.uint8)
+        img = np.vstack((before_col, img, after_col))
+
+    # BGR to RGB
+    img = img[..., (2, 1, 0, 3)]
+    img = cv2.resize(img, (img_size, img_size))
+    img = Image.fromarray(img)
+    img.save(dst_file)
+
 def download_img(data, path, img_size):
     source = data['image']['source']
     commit = data['image']['commit']
-    code = data['image']['unicodeCodePoint'][0]
-    code = hex(ord(code))[2:]
+    code = data['image']['unicodeCodePoint']
+    code = '_'.join([hex(ord(c))[2:] for c in code])
     img_type = data['id'].split('-')[0]
     if not img_type.endswith('s'):
         img_type += 's'
     dst_file = f"imgs/{img_type}/{data['name']}.png"
+
+    if data['name'] in SAP_WIKI_URL:
+        download_from_wiki(data['name'], dst_file, img_size)
+        return
 
     if data['name'] in WRONG_COMMITS:
         git_dir = os.path.join(path, source)
@@ -57,16 +93,12 @@ def download_imgs(file_name, path, img_size):
     with open(file_name, 'r') as file:
         data = json.load(file)
 
+    os.makedirs("imgs/pets", exist_ok=True)
     for pet_data in data['pets'].values():
-        os.makedirs("imgs/pets", exist_ok=True)
         download_img(pet_data, path, img_size)
 
-    for food_data in data['foods'].values():
-        os.makedirs("imgs/foods", exist_ok=True)
-        download_img(food_data, path, img_size)
-
+    os.makedirs("imgs/status", exist_ok=True)
     for status_data in data['statuses'].values():
-        os.makedirs("imgs/status", exist_ok=True)
         download_img(status_data, path, img_size)
 
 
